672b3876eb7b9d46db8f0de1c342d1df4c33f1be
YOUR_NAME <steve3341@DESKTOP-DEMPQ84>
2023-12-15 14:21:54 -0500

    Describe your changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {Gallary} from "./components/gallary"
 import {Contact} from './components/contaccts'
 import {Main} from './components/layout'
 import {AppointmentBooking} from './components/booknow'
+import { Employee } from './components/employee'
 function App() {
 
 
@@ -12,8 +13,11 @@ function App() {
   <>
 
   <Router>
+
   <h1><Main/></h1>
+  
 <Routes>
+  <Route path="/employee" element={<Employee/>} />
  <Route path="/menu" element={<Menu/>} />
  <Route path="/gallary" element={<Gallary/>} />
  <Route path="/contact" element={<Contact/>} />
---
+++ src/components/employee.jsx
@@ -0,0 +1,46 @@
+
+import '../App.css'
+import { AddEmployee } from './utility'
+
+export const Employee = () => {
+
+  const [name,setName] = useState("")
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const success = await AddEmployee(name);
+      if (success) {
+        console.log('add employee success');
+      } else {
+        console.log('add employee failed');
+      }
+    } catch (error) {
+      console.error('Error during sign up:', error);
+    }
+    
+    setName("");
+   
+  };
+
+  return (
+  <div className='body'>
+  <div className="form-container">
+  
+  
+  <h2>Add New Employee</h2>
+  <form onSubmit = {handleSubmit} className ='form' >
+
+      <label className = 'lable' for="name">Name:</label>
+      <input type="text" id="name" name="name" onChange={(e)=>setName(e.target.value)} value={name}/>
+  
+      <input className = 'input' type="submit" value="Add Employee"/>
+  </form>
+  
+  
+  </div>
+  
+  </div>
+  )
+  
+  }
