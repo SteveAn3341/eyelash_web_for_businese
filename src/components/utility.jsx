fdcff38cce0cc3fecdc45bda7bfe2bf3b05ff5b5
YOUR_NAME <steve3341@DESKTOP-DEMPQ84>
2023-12-16 00:50:31 -0500

    removed

--- src/components/utility.jsx
+++
@@ -1,9 +0,0 @@
-import { getToken } from "./csrftoken"
-
-
-export const AddEmployee = async () =>{
-  getToken()
-  const response = await axios.get(`/employee/`)
-  console.log(response.data)
-  return response.data
-}
